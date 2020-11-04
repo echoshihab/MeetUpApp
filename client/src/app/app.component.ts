c832cbf24b3122c733efdb1530a587091ddee7ff
shihab <echoshihab@gmail.com>
2020-11-03 19:00:01 -0500

    demo parent to child com via register component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -12,10 +12,9 @@ export class AppComponent implements OnInit{
   title = 'The Meetup App';
   users: any;
 
-  constructor(private http: HttpClient, private accountService: AccountService) {}
+  constructor(private accountService: AccountService) {}
 
   ngOnInit() {
-    this.getUsers();
     this.setCurrentUser();
   }
 
@@ -24,13 +23,7 @@ export class AppComponent implements OnInit{
     this.accountService.setCurrentUser(user);
   }
 
-  getUsers(){
-    this.http.get('https://localhost:5001/api/users').subscribe(response => {
-      this.users = response;
-    }, error => {
-      console.log(error);
-    })
-  }
+  
 }
 
 
