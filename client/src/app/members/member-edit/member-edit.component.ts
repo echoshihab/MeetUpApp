b4d9c8898330d9f00ef279960ca8c83f3241d304
shihab <echoshihab@gmail.com>
2020-11-17 05:29:04 -0500

    hook up client update user to api

--- client/src/app/members/member-edit/member-edit.component.ts
+++ client/src/app/members/member-edit/member-edit.component.ts
@@ -41,9 +41,11 @@ loadMember(){
 }
 
 updateMember(){
-  console.log(this.member);
-  this.toastr.success('Profile updated successfully');
-  this.editForm.reset(this.member)
+  this.memberService.updateMember(this.member).subscribe(()=> {
+    this.toastr.success('Profile updated successfully');
+    this.editForm.reset(this.member)
+  })
+
 
 }
 
