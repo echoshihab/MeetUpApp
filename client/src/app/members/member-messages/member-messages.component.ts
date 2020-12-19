4bbe4ebfc2cd272da1b5918b71d744b2488fc4c0
shihab <echoshihab@gmail.com>
2020-12-19 10:55:23 -0500

    style the message thread

--- client/src/app/members/member-messages/member-messages.component.ts
+++ client/src/app/members/member-messages/member-messages.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Message } from 'src/app/_models/message';
 import { MessageService } from 'src/app/_services/message.service';
 
 @Component({
