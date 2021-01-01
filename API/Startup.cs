fe776e97b0a499394b883fcbca8848817fa8de07
shihab <echoshihab@gmail.com>
2021-01-01 13:39:30 -0500

    add custom signalR hub

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,6 +1,7 @@
 
 using API.Extensions;
 using API.MiddleWare;
+using API.SignalR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 
@@ -29,6 +30,7 @@ namespace API
             services.AddControllers();
             services.AddCors();
             services.AddIdentityServices(_config);
+            services.AddSignalR();
 
         }
 
@@ -56,6 +58,7 @@ namespace API
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapHub<PresenceHub>("hub/presence");
             });
         }
     }
