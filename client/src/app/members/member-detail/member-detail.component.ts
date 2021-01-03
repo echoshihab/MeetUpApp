115b3cfacb3bc9b4abd6c29263fa41e51a61a158
shihab <echoshihab@gmail.com>
2021-01-03 12:02:07 -0500

    display online presence on member details page

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -5,8 +5,8 @@ import { NgxGalleryAnimation, NgxGalleryImage, NgxGalleryOptions } from '@kolkov
 import { TabDirective, TabsetComponent } from 'ngx-bootstrap/tabs';
 import { Member } from 'src/app/_models/member';
 import { Message } from 'src/app/_models/message';
-import { MembersService } from 'src/app/_services/members.service';
 import { MessageService } from 'src/app/_services/message.service';
+import { PresenceService } from 'src/app/_services/presence.service';
 
 @Component({
   selector: 'app-member-detail',
@@ -21,7 +21,7 @@ export class MemberDetailComponent implements OnInit {
   activeTab: TabDirective;
   messages: Message[] = [];
 
-  constructor(private memberservice: MembersService, private route: ActivatedRoute, 
+  constructor(public presence: PresenceService, private route: ActivatedRoute, 
     private messageService: MessageService) { }
 
   ngOnInit(): void {
