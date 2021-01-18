e6a48d7c6694cf089cd176be1aed301e57c87fd6
shihab <echoshihab@gmail.com>
2021-01-17 19:59:59 -0500

    optimize query for retrieving user gender

--- API/Interfaces/IUserRepository.cs
+++ API/Interfaces/IUserRepository.cs
@@ -20,5 +20,7 @@ namespace API.Interfaces
         Task<PagedList<MemberDto>> GetMembersAsync(UserParams userParams);
         Task<MemberDto> GetMemberAsync(string username);
 
+        Task<string> GetUserGender(string username);
+
     }
 }
